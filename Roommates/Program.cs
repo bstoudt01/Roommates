98b0c38c69ccffe1c1fdd1ec069af96f986e1282
Brett Stoudt <bstoudt01@gmail.com>
2020-09-08 14:07:26 -0500

    completed edit/update and delete methods. i see some similarities to how js and json worked together...

--- Roommates/Program.cs
+++ Roommates/Program.cs
@@ -17,6 +17,7 @@ namespace Roommates
         {
             RoomRepository roomRepo = new RoomRepository(CONNECTION_STRING);
 
+            //GET ALL ROOMS
             Console.WriteLine("Getting All Rooms:");
             Console.WriteLine();
 
@@ -27,12 +28,43 @@ namespace Roommates
                 Console.WriteLine($"{room.Id} {room.Name} {room.MaxOccupancy}");
             }
 
+            //GET SINGLE ENTRY
             Console.WriteLine("----------------------------");
             Console.WriteLine("Getting Room with Id 1");
-
             Room singleRoom = roomRepo.GetById(1);
 
             Console.WriteLine($"{singleRoom.Id} {singleRoom.Name} {singleRoom.MaxOccupancy}");
+
+            //ADD ENTRY
+            Room bathroom = new Room
+            {
+                Name = "Bathroom",
+                MaxOccupancy = 1
+            };
+
+
+            roomRepo.Insert(bathroom);
+
+            Console.WriteLine("-------------------------------");
+            Console.WriteLine($"Added the new Room with id {bathroom.Id}");
+
+
+            //UPDATE object above"bathroom" on the property "maxoccupency" and set it equal to "3"...
+            bathroom.MaxOccupancy = 3;
+            roomRepo.Update(bathroom);
+            //get it from database
+            Room bathroomFromDB = roomRepo.GetById(bathroom.Id);
+            //..then see it on the console
+            Console.WriteLine($"{bathroomFromDB.Id} {bathroomFromDB.Name} {bathroomFromDB.MaxOccupancy}");
+
+
+            //DELETE ENTRY BY ID
+            roomRepo.Delete(bathroom.Id);
+            //get all rooms and loop through them to show its deleted by delting the SQL assigned id of the entry
+            allRooms = roomRepo.GetAll();
+            foreach(Room room in allRooms){ 
+            Console.WriteLine($"{room.Id} {room.Name} {room.MaxOccupancy}");
+            }
 
         }
     }
--- Roommates/Repositories/RoomRepository.cs
+++ Roommates/Repositories/RoomRepository.cs
@@ -114,6 +114,80 @@ namespace Roommates.Repositories
                 }
             }
         }
+
+        /// <summary>
+        ///  Add a new room to the database
+        ///   NOTE: This method sends data to the database,
+        ///   it does not get anything from the database, so there is nothing to return.
+        /// </summary>
+        public void Insert(Room room)
+        {
+            using (SqlConnection conn = Connection)
+            {
+                conn.Open();
+                using (SqlCommand cmd = conn.CreateCommand())
+                {
+                    // These SQL parameters are annoying. Why can't we use string interpolation?
+                    // ... sql injection attacks!!!
+                    cmd.CommandText = @"INSERT INTO Room (Name, MaxOccupancy) 
+                                         OUTPUT INSERTED.Id 
+                                         VALUES (@name, @maxOccupancy)";
+                    cmd.Parameters.AddWithValue("@name", room.Name);
+                    cmd.Parameters.AddWithValue("@maxOccupancy", room.MaxOccupancy);
+                    int id = (int)cmd.ExecuteScalar();
+                    //The cmd.ExecuteScalar method does two things: First, it executes the SQL command against the database. 
+                    //Then it looks at the first thing that the database sends back (in our case this is just the Id it created for the room) and returns it.
+                    // ....room.id declared here now that the result has been returned (similar to seeing the result when using .then statements
+                    //with the rresult returned we have the Id that the SQL server assigned
+                    room.Id = id;
+                }
+            }
+
+            // when this method is finished we can look in the database and see the new room.
+        }
+
+        /// <summary>
+        ///  Updates the room
+        /// </summary>
+        public void Update(Room room)
+        {
+            using (SqlConnection conn = Connection)
+            {
+                conn.Open();
+                using (SqlCommand cmd = conn.CreateCommand())
+                {
+                    cmd.CommandText = @"UPDATE Room
+                                    SET Name = @name,
+                                        MaxOccupancy = @maxOccupancy
+                                    WHERE Id = @id";
+                    cmd.Parameters.AddWithValue("@name", room.Name);
+                    cmd.Parameters.AddWithValue("@maxOccupancy", room.MaxOccupancy);
+                    cmd.Parameters.AddWithValue("@id", room.Id);
+
+                    cmd.ExecuteNonQuery();
+                    //We use this method when we want to execute a SQL command, but we don't expect anything back from the database.
+                }
+            }
+        }
+
+        /// <summary>
+        ///  Delete the room with the given id
+        /// </summary>
+        public void Delete(int id)
+        {
+            using (SqlConnection conn = Connection)
+            {
+                conn.Open();
+                using (SqlCommand cmd = conn.CreateCommand())
+                {
+                    cmd.CommandText = "DELETE FROM Room WHERE Id = @id";
+                    cmd.Parameters.AddWithValue("@id", id);
+                    cmd.ExecuteNonQuery();
+                }
+            }
+        }
+
+
     }
 
 }
